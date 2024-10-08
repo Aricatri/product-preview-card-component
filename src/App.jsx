61eec50f1985b74d0d99ca0c32b248cebe439466
Aricatri <aricatri@gmail.com>
2024-10-08 02:46:39 -0500

    feat:appFeature

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import perfume1 from './components/img/image-product-desktop.jpg';
+import perfume2 from './components/img/image-product-mobile.jpg';
+import ProductDescription from './components/ProductDescription';
+import AddPurchase from './components/AddProduct';
+import ProductPrice from './components/ProductPrice';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="card-container">
+      <div className="card-image-container">
+        <img className="card-image1" src={perfume1} alt="perfumeDesktop" />
+        <img className='card-image2' src={perfume2} alt='perfumePhone' />
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p className="sub-title">PERFUME</p>
+        <div className="card-body">
+          <h2 className="card-title">Gabrielle Essence Eau De Parfum</h2>
+          <div className="plan-details">
+            <ProductDescription/>
+          </div>
+          <div className="action-buttons">
+            <ProductPrice/>
+            <AddPurchase/>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
